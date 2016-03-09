198f955bfd0a91e5dd1ee0e1be2884fc306a66db
Indah WA <Indah WA>
2016-03-09 22:03:55 +0700

    proses 4 di main program

--- src/MahasiswaLapangan.java
+++ src/MahasiswaLapangan.java
@@ -1,7 +1,21 @@
 package Tugas;
 
+import java.util.Scanner;
+
 public class MahasiswaLapangan {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner (System.in);
+
+	int jumlah_mahasiswa;
+	double pjg = 0;
+	double lebar = 0;
+	
+	System.out.print("Jumlah mahasiswa    : ");
+	jumlah_mahasiswa = in.nextInt();
+
+	System.out.println();
+	
+
+
     }
 }
