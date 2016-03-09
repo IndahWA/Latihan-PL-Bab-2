4bfb2b44e96455a2ed6fefecf57cc3e368012055
Indah WA <Indah WA>
2016-03-09 21:45:28 +0700

    proses hitung lapangan

--- src/Lapangan.java
+++ src/Lapangan.java
@@ -1,5 +1,15 @@
 package Tugas;
 
 public class Lapangan {
-    
+	double pjg;
+	double lebar;
+
+	public Lapangan(double pjg, double lebar){
+	this.pjg = pjg;
+	this.lebar = lebar;
+	}
+
+	public double kelilingLapangan(){
+	return 2 * (pjg + lebar);
+	}
 }
