15164ff2996bb4b33816f13019df5efaa145992e
Indah WA <Indah WA>
2016-03-09 21:37:20 +0700

    proses

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -2,4 +2,17 @@ package Tugas;
 
 public class Mahasiswa {
     
+	String nim;
+	String nama_mahasiswa;
+	double jumlahPutaran;
+	double jumlah_waktu;
+
+	public Mahasiswa(String nim, String nama_mahasiswa, double jumlahPutaran, double jumlah_waktu){
+	this.nim = nim;
+	this.nama_mahasiswa = nama_mahasiswa;
+	this.jumlahPutaran;
+	this.jumlah_waktu;
+	}
+
+
 }
